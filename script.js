691677ad90900eb6e8898c88c6e2ee8bb9e8b521
Pratiksh Saini <pratikshsaini10@gmail.com>
2025-07-14 14:57:01 +0530

    Third problem solved

--- script.js
+++ script.js
@@ -6,10 +6,29 @@ console.log(evenArr);
 
 // ...............................................
 
-let values = [0 , "hello", false, 42, "", null]
-let truthy = values.filter(Boolean)
+let values = [0, "hello", false, 42, "", null];
+let truthy = values.filter(Boolean);
 console.log(truthy);
 
 // ...............................................
 
+let user = [
+  { name: "A", age: 16 },
+  { name: "B", age: 20 },
+];
+let adult = user.filter((x) => {
+  if (x.age > 18) {
+    return x;
+  }
+});
+console.log(adult);
+
+// or (Another Way)
+
+let adult2 = user.filter((x) => {
+  return x.age > 18;
+});
+console.log(adult2);
+
+// ...............................................
 
